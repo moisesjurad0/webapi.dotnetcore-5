a558839ec21f5e17d7829c3e0e42a34a294cf219
Moisés Jurado <moises003@gmail.com>
2023-06-09 19:09:55 -0500

    modificaciones para el contexto

---
+++ myAPI/Pet.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace myAPI
+{
+    public class Pet
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { set; get; }
+
+        [Required]
+        public string? Name { set; get; }
+
+        public int Paws { set; get; }
+    }
+}
