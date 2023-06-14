dc967637fd4be38a15772a0def06faf991de7322
Moisés Jurado <moises003@gmail.com>
2023-06-14 16:52:25 -0500

    giving a try webapp identity scafold

---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=db;Port=5432;Database=mydatabase;User Id=postgres;Password=password;");
+        }
+    }
+}
